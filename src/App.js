aa31e8c50dd6a49b6a1861c45fdfa863d1714306
carlos <carlosjleonel@hotmail.com>
2018-10-04 12:09:04 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../node_modules/semantic-ui/dist/semantic.min.css'
+//import $ from  '../node_modules/jquery/dist/jquery.min.js'
+//import '../node_modules/semantic-ui/dist/semantic.min.js'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default App;
