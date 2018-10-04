5250baed7d2d7f0cdc988fab64a045efc3d94461
carlos <carlosjleonel@hotmail.com>
2018-10-04 12:30:18 -0300

    Creating login component and routers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
-import '../node_modules/semantic-ui/dist/semantic.min.css'
-//import $ from  '../node_modules/jquery/dist/jquery.min.js'
-//import '../node_modules/semantic-ui/dist/semantic.min.js'
+import {BrowserRouter,Switch,Route}  from 'react-router-dom';
 import './App.css';
+
+import Login from './Components/Login/Login'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <BrowserRouter>
+          <Switch>
+            <Route path="/login" exact component={Login} />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
