3a68dd10bdcb5223488cdf1d964e3c610cfd49f5
carlos <carlosjleonel@hotmail.com>
2018-10-04 23:17:16 -0300

    feat chargedb method in the app component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import {BrowserRouter,Switch,Route}  from 'react-router-dom';
 import './App.css';
-
 import Login from './Components/Login/Login'
 
+import {chargeDB} from './connectDatabase';
+
 class App extends Component {
+  
+  async componentDidMount() {
+    chargeDB()
+  }
+
   render() {
     return (
       <div className="App">
