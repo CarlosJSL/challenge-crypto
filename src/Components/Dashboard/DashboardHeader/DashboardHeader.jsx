5563f7c3f0f56fd580f887e409cdb66b861dde91
carlos <carlosjleonel@hotmail.com>
2018-10-06 17:33:02 -0300

    remove balance on header

--- src/Components/Dashboard/DashboardHeader/DashboardHeader.jsx
+++ src/Components/Dashboard/DashboardHeader/DashboardHeader.jsx
@@ -11,12 +11,6 @@ export default () => (
             </a>
             <a href="#" className="item">
                 <i className="sign-out icon"></i>
-            </a><a href="#" className="item">
-                
-                Saldo: 
-                R$ {parseFloat(JSON.parse(window.localStorage.getItem('user')).wallet.real_value).toFixed(2).toLocaleString('br')}   | 
-                ฿: {parseFloat(JSON.parse(window.localStorage.getItem('user')).wallet.bitcoin_value).toLocaleString('pt')}     | 
-                $: {parseFloat(JSON.parse(window.localStorage.getItem('user')).wallet.brita_value).toLocaleString('en')}
             </a>
             <a href="#" className="item">
                 Bem vindo, {JSON.parse(window.localStorage.getItem('user')).name}!
