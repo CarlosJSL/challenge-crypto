439a369e1788aac04b03ba1359572e74d888862a
carlos <carlosjleonel@hotmail.com>
2018-10-07 11:15:40 -0300

    Add props in the component

--- src/Components/Dashboard/DashboardContent/DashboardContent.jsx
+++ src/Components/Dashboard/DashboardContent/DashboardContent.jsx
@@ -1,34 +1,51 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './DashboardContent.css'
+import { getUserAmount } from '../../../connectDatabase'
 
-export default () => (
-    <div className="ui grid">
-        <div className="sixteen wide column">
-            <h1>Patrimônio Total</h1>
-                <br></br>
-                <div className="ui  one column centered grid">
-                    <div className="ui statistics">
-                        <div className="statistic">
-                            <div className="value"> 
-                                {parseFloat(JSON.parse(window.localStorage.getItem('user')).wallet.real_value).toFixed(2).toLocaleString('br')} 
+export default class DashboardContent extends Component{
+
+    constructor(props){
+        super(props)
+        this.state ={
+            userCryptoAmount:''
+        }
+    }
+
+    async componentDidMount(){
+        this.setState({...this.state, userCryptoAmount: await getUserAmount('user')})
+    }
+
+    render () {
+        return (
+            <div  className="ui grid">
+                <div className="sixteen wide column">
+                    <h1>{this.state.onPush}</h1>
+                        <br></br>
+                        <div className="ui  one column centered grid">
+                            <div className="ui statistics">
+                                <div className="statistic">
+                                    <div className="value"> 
+                                        {this.props.userCryptoAmount.real_value || this.state.userCryptoAmount.real_value} 
+                                    </div>
+                                    <div className="label"> R$ </div>
+                                </div>
+                                <div className="statistic">
+                                    <div className="value"> 
+                                        {this.props.userCryptoAmount.bitcoin_value  || this.state.userCryptoAmount.bitcoin_value} 
+                                    </div>
+                                    <div className="label"> ฿ </div>
+                                </div>
+                                <div className="statistic">
+                                    <div className="value"> 
+                                        {this.props.userCryptoAmount.bitcoin_value || this.state.userCryptoAmount.brita_value} 
+                                    </div>
+                                    <div className="label"> $ </div>
+                                </div>
                             </div>
-                            <div className="label"> R$ </div>
-                        </div>
-                        <div className="statistic">
-                            <div className="value"> 
-                                {parseFloat(JSON.parse(window.localStorage.getItem('user')).wallet.bitcoin_value).toLocaleString('pt')}  
-                            </div>
-                            <div className="label"> ฿ </div>
-                        </div>
-                        <div className="statistic">
-                            <div className="value"> 
-                                {parseFloat(JSON.parse(window.localStorage.getItem('user')).wallet.brita_value).toLocaleString('en')} 
-                            </div>
-                            <div className="label"> $ </div>
-                        </div>
                     </div>
-               </div>
-               <br></br>
-        </div>
-    </div>
-)
+                    <br></br>
+                </div>
+            </div>
+        )
+    }
+} 
