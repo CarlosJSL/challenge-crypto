dc6642d97f65693e9de80e65658765c74da80325
carlos <carlosjleonel@hotmail.com>
2018-10-07 11:18:02 -0300

    Add j query dependency

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import '../node_modules/semantic-ui/dist/semantic.min.css'
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+window.$ = window.jQuery = require('jquery')
+require('../node_modules/semantic-ui/dist/semantic.min.js')
 
 ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
