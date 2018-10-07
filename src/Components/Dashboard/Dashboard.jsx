ea12d98bf0bf24d409cd6a030976d22b55603da7
carlos <carlosjleonel@hotmail.com>
2018-10-07 11:15:03 -0300

    Add lifecycle in the Dashboard component

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './Dashboard.css'
 import DashboardHeader from './DashboardHeader/DashboardHeader'
 import DashboardContent from './DashboardContent/DashboardContent'
 import DashboardTrade from './DashboardTrade/DashboardTrade'
 
-export default () => (    
-    <div>
-        <DashboardHeader />
-        <DashboardContent />
-        <DashboardTrade />
-    </div>
-)
+//import { getUserAmount } from '../../connectDatabase'
+
+export default class Dashboard extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            userCryptoAmount: ''
+        }
+        this.getUserCryptoAmount = this.getUserCryptoAmount.bind(this)
+    }
+
+    getUserCryptoAmount(){
+        this.setState({userCryptoAmount: {real_value:10, bitcoin_value:14, brita_value:12}})
+    }
+    
+    render() {
+        return (
+            <div>
+                <DashboardHeader />
+                <DashboardContent userCryptoAmount = {this.state.userCryptoAmount} />
+                <DashboardTrade  user = {this.getUserCryptoAmount}/>
+            </div>
+        )
+    }
+} 
