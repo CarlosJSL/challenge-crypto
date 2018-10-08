07de3a96baa5ce5fa7e07df686ca6ca239716c64
carlos <carlosjleonel@hotmail.com>
2018-10-08 17:49:33 -0300

    feat logout

--- src/Components/Dashboard/DashboardHeader/DashboardHeader.jsx
+++ src/Components/Dashboard/DashboardHeader/DashboardHeader.jsx
@@ -1,20 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './DashBoardHeader.css'
 import stoneSvg from '../../../images/stone.svg'
 
-export default () => (
-    <div className = "ui fixed inverted menu">
-        <div className = "ui container">
-            <a href="#" className="header item"> 
-                <img  src={stoneSvg}>
-                </img>
-            </a>
-            <a href="#" className="item">
-                <i className="sign-out icon"></i>
-            </a>
-            <a href="#" className="item">
-                Bem vindo, {JSON.parse(window.localStorage.getItem('user')).name}!
-            </a>
-        </div>
-    </div>
-)
+export default class DashboardHeader extends Component {
+
+    logout() {
+        window.localStorage.removeItem("user")
+        window.location.reload()
+    }
+
+    render() {
+        return (
+            <div className = "ui fixed inverted menu">
+                <div className = "ui container">
+                    <a href="#" className="header item"> 
+                        <img  src={stoneSvg}>
+                        </img>
+                    </a>
+                    <a onClick = {() => this.logout()} href="#" className="item">
+                        <i className="sign-out icon"></i>
+                    </a>
+                    <a href="#" className="item">
+                        Bem vindo, {JSON.parse(window.localStorage.getItem('user')).name||''}!
+                    </a>
+                </div>
+            </div>
+        )
+    }
+}
