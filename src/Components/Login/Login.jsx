7fa32fe9cffe8d47dfeba9256f664402dd6e432c
carlos <carlosjleonel@hotmail.com>
2018-10-10 00:20:33 -0300

    fix imports

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import './Login.css'
-import stoneSvg from '../../images/stone.svg'
-import LoginForm from './LoginForm/LoginForm'
+import React from 'react';
+import './Login.css';
+import stoneSvg from '../../images/stone.svg';
+import LoginForm from './LoginForm/LoginForm';
 
 export default (props) => (
     <div className="login-main">
         <div className="ui one column centered grid">
                 <img className="logo" src={stoneSvg}></img>
         </div>
-        
         <LoginForm router = {props} />
     </div>
 )
