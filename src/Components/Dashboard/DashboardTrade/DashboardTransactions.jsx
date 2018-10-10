37b9ce49b0ddf3d69b1b372ab3c9c9730e8d21d5
carlos <carlosjleonel@hotmail.com>
2018-10-10 00:17:34 -0300

    fix html element

--- src/Components/Dashboard/DashboardTrade/DashboardTransactions.jsx
+++ src/Components/Dashboard/DashboardTrade/DashboardTransactions.jsx
@@ -2,53 +2,40 @@ import React, { Component } from 'react';
 
 import './DashboardTransactions.css';
 
-export default class DashboardTransactions extends Component {
 
-    constructor (props) {
-        super(props)
-        this.state = {
-            transactions: []
-        }
-    }
-    render (){
-        
-            return (
-                <table className="ui very basic collapsing celled table">
-                        <thead>
-                            <tr>
-                                <th>Trocou</th>
-                                <th>Por</th>
+export default (props) => (
+    <table className="ui very basic collapsing celled table">
+            <thead>
+                <tr>
+                    <th>Trocou</th>
+                    <th>Por</th>
+                </tr>
+            </thead>
+            <tbody>
+                { 
+                    props.transactions.map((transaction,index) => { 
+                        return (
+                            <tr key ={index}>
+                            <td>
+                                <h4 className="ui image header">
+                                    <div className="content">
+                                        {transaction.sell.totalValue} {transaction.sell.crypto}
+                                        <div className="sub header">{transaction.date}</div>
+                                    </div>
+                                </h4>
+                            </td>
+                            <td>
+                                <h4 className="ui image header">
+                                    <div className="content">
+                                        {transaction.buy.totalValue} {transaction.buy.crypto}
+                                        <div className="sub header">{transaction.date}</div>
+                                    </div>
+                                </h4>
+                            </td>
                             </tr>
-                        </thead>
-                        <tbody>
-                            { 
-                                this.props.transactions.map((transaction,index) => { 
-                                    return (
-                                        <tr key ={index}>
-                                        <td>
-                                          <h4 className="ui image header">
-                                                {/* <img src="https://en.bitcoin.it/w/images/en/2/29/BC_Logo_.png" className="ui mini rounded image"></img> */}
-                                                <div className="content">
-                                                    {transaction.sell.totalValue} {transaction.sell.crypto}
-                                                    <div className="sub header">{transaction.date}</div>
-                                                </div>
-                                            </h4>
-                                        </td>
-                                        <td>
-                                            <h4 className="ui image header">
-                                                {/* <img src="http://drawinglondon.com/wp-content/uploads/2018/01/how-to-draw-a-dollar-sign-money-sign-drawing-how-to-draw-a-dollar-sign-in-3d-youtube.jpg" className="ui mini rounded image"></img> */}
-                                                <div className="content">
-                                                    {transaction.buy.totalValue} {transaction.buy.crypto}
-                                                    <div className="sub header">{transaction.date}</div>
-                                                </div>
-                                            </h4>
-                                        </td>
-                                      </tr>
-                                    )
-                                })
-                            }
-                        </tbody>
-                </table>
-            )
-    }
-}
+                        )
+                    })
+                }
+            </tbody>
+    </table>
+)
